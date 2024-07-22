098c5ccd23f9cf891a8857596b39caf35be0b08d
BharteshGirmal <106860439+BharteshGirmal@users.noreply.github.com>
2024-07-22 20:13:29 +0530

    Update Email2.pl

--- PERL/Sending Email/Email2.pl
+++ PERL/Sending Email/Email2.pl
@@ -5,8 +5,8 @@ use warnings;
 use Mime::Lite;
 
 my $mail -MIME::Lite->new(
-            From =>'sp922989.ttl@tatamotors.com',
-            to => 'bg925781.ttl@tatamotors.com',
+            From =>'bharteshgirmal@gmail.com',
+            to => 'bharteshgirmal@gmail.com',
             Subject => "Report",
             Type => 'multipart/mixed'
 );
